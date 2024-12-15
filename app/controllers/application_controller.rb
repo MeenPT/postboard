2df56b5dd9516aaa95a24322f510010f4adfe945
Pakapol Tanachaisith <meen260600@gmail.com>
2024-12-15 14:27:09 +0700

    Styles Signin and Sigup pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,21 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  layout :layout_by_resource
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
   end
 end
