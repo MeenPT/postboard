5f7500f3ad444073462911ac41c239739003a25c
Pakapol Tanachaisith <meen260600@gmail.com>
2024-12-15 13:02:48 +0700

    Install and configure  Devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username,
+            presence: true,
+            length: { in: 4...50 },
+            uniqueness: { case_sensitive: false }
+end
